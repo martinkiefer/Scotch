0c00452a4d4246ca7052b1cee7b16d81c55ee512
Martin Kiefer <martinkiefer@boxm.de>
2020-09-18 01:13:41 +0200

    Added xcvu7p-flva2104-3-e-flex-1-32-6 I/O template.

---
+++ IO-Controllers/xcvu7p-flva2104-3-e-flex-1-32-6/generate.tcl
@@ -0,0 +1,27 @@
+set outputdir ./
+create_project -part xcvu7p-flva2104-3-e cproject $outputdir
+add_files [glob ./sketch/*.vhd]
+add_files -fileset constrs_1 ./constraints.xdc 
+add_files ./tl.vhd
+set_property top tl [current_fileset]
+set_property STEPS.SYNTH_DESIGN.ARGS.KEEP_EQUIVALENT_REGISTERS true [get_runs synth_1]
+set_property STEPS.SYNTH_DESIGN.ARGS.FLATTEN_HIERARCHY none [get_runs synth_1]
+
+if { "[lindex $argv 0]" == "1" } {
+    set_property strategy Performance_Explore [get_runs impl_1]
+} elseif { "[lindex $argv 0]" == "2" } {
+    set_property strategy Performance_ExplorePostRoutePhysOpt [get_runs impl_1]
+} elseif { "[lindex $argv 0]" == "3" } {
+    set_property strategy Performance_NetDelay_low [get_runs impl_1]
+} elseif { "[lindex $argv 0]" == "4" } {
+    set_property strategy Performance_Retiming [get_runs impl_1]
+} else {
+    set_property strategy Performance_RefinePlacement [get_runs impl_1]
+}
+
+launch_runs synth_1 -jobs 20
+wait_on_run synth_1
+launch_runs impl_1 -jobs 20
+wait_on_run impl_1
+open_run impl_1
+report_utilization -hierarchical  -file utilization_hierarchy.rpt
---
+++ IO-Controllers/xcvu7p-flva2104-3-e-flex-1-32-6/tl.vhd
@@ -0,0 +1,69 @@
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use WORK.functions_pkg.all;
+
+ENTITY tl IS
+PORT (
+clk : in std_logic;
+sin1 : in std_logic;
+sout1 : out std_logic
+);
+END tl;
+ARCHITECTURE atl OF tl IS
+    signal input_chain : std_logic_vector(34 downto 0);
+
+COMPONENT sketch IS
+PORT (
+clk : in std_logic;
+rd_en_in : in std_logic;
+rd_data_out : out std_logic_vector(5 downto 0);
+rd_valid_out : out std_logic;
+val0_en_in : in std_logic;
+val0_in : in std_logic_vector(31 downto 0)
+);
+END COMPONENT sketch;signal sketch_clk : std_logic;
+signal sketch_rd_en_in : std_logic;
+signal sketch_rd_data_out : std_logic_vector(5 downto 0);
+signal sketch_rd_valid_out : std_logic;
+signal sketch_val0_en_in : std_logic;
+signal sketch_val0_in : std_logic_vector(31 downto 0);
+
+BEGIN
+sketch_rd_en_in <= input_chain(0);
+sketch_val0_en_in <= input_chain(1);
+sketch_val0_in <= input_chain(33 downto 2);
+
+ssketch: sketch
+PORT MAP(
+
+clk => sketch_clk,
+rd_en_in => sketch_rd_en_in,
+rd_data_out => sketch_rd_data_out,
+rd_valid_out => sketch_rd_valid_out,
+val0_en_in => sketch_val0_en_in,
+val0_in => sketch_val0_in
+);
+
+
+sketch_clk <= clk;
+
+process(clk)
+begin
+	if rising_edge(clk) then
+		input_chain(0) <= sin1;
+		for i in 0 to input_chain'LENGTH-2 loop
+			input_chain(i+1) <= input_chain(i);
+		end loop;
+	end if;
+end process; 
+
+process(clk)
+begin
+	if rising_edge(clk) then
+		sout1 <= parity(sketch_rd_data_out) xor sketch_rd_valid_out;
+	end if;
+end process;
+
+END atl;
